a8ef23552591b00635510fb3f78086c81c9067d6
Carlos Mejia <cdmejia4@utpl.edu.ec>
2023-05-16 17:40:14 -0500

    subida final

--- ejemplos/mis_clases.py
+++ ejemplos/mis_clases.py
@@ -8,6 +8,76 @@
 
 # clase 01
 
+class Auto:
+    def __init__(self, cilindraje, gama, precio, marca):
+        self.cilindraje = cilindraje
+        self.gama = gama
+        self.precio = precio
+        self.marca = marca
+       
+    def establecerCilindraje (self, cili):
+        cilindraje = cili
+    def establecerGama (self, gam):
+        gama = gam
+
+    def establecerPrecio (self,preci):
+        precio = preci
+
+    def establecerMarca (self, marc):
+        marca = marc
+
+
+    def obtenerCilindraje (self):
+        return self.cilindraje
+    
+    def obtenerGama (self):
+        return self.gama
+
+    def obtenerPrecio (self):
+        return self.precio
+
+    def obtenerMarca (self):
+        return self.marca
+
+    def __str__(self):
+        mensaje = (f"El auto de marca: {self.marca}\nCilindraje: {self.cilindraje} CC\nGama: {self.gama}\nPrecio: ${self.precio}")
+        return mensaje
 
 
 # clase 02
+class Avion:
+    def __init__(self, marca:str, aerolinea:str, tipo:str, precio:float):
+        self.marca = marca
+        self.aerolinea = aerolinea
+        self.tipo_avion = tipo
+        self.precio = precio
+    
+    def establecerMarca (self, marc):
+        marca = marc
+    def establecerAerolinea (self, aero):
+        aerolinea = aero
+
+    def establecerPrecio (self,preci):
+        precio = preci
+
+    def establecerTipo (self, tip):
+        tipo_avion = tip
+
+
+    def obtenerMarca (self):
+        return self.marca
+    
+    def obtenerAerolinea (self):
+        return self.aerolinea
+
+    def obtenerTipo (self):
+        return self.tipo_avion
+
+    def obtenerPrecio (self):
+        return self.precio 
+
+    
+
+    def __str__(self):
+        mensaje = (f"Avion de marca: {self.marca}\nAerolinea: {self.aerolinea} \nTipo: {self.tipo_avion}\nPrecio: $ {self.precio}")
+        return mensaje
--- ejemplos/run.py
+++ ejemplos/run.py
@@ -1,18 +1,28 @@
 """
 
 """
+import mis_clases
+
 # Crear dos objetos de la clase 01
-
 # objeto 01
-
 # crear
 
+carro1 = mis_clases.Auto(6300, "Lujo", 145500, "Mercedez Benz")
+
+
 # Presentar objeto; usar el método __st__
+print (carro1)
 
 # objeto 02
-
 # crear ingresando valores por teclado
 
+marca = input (str("Ingrese la marca del avion: "))
+aerolinea = input (str("Ingrese la aerolinea del avion: "))
+tipo = input (str("Ingrese el tipo de avion: "))
+precio =  float (input("Ingrese el precio aproximado del avion: "))
+
+
+avion1 = mis_clases.Avion(marca, aerolinea, tipo, precio)
+
 # Presentar objeto; usar el método __st__
-
-
+print (avion1)
